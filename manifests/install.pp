855a2b3d8947e1116ba1c88318fd47402042746f
Justin Bronn <justin@counsyl.com>
2014-01-27 19:26:59 -0800

    Have `apache::install` inherit from `apache::params` and allow overriding of the installation package.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,21 +2,31 @@
 #
 # Installs the Apache 2 web server.
 #
-class apache::install {
-  include apache::params
+# === Parameters
+#
+# [*package*]
+#  The package for Apache2, default is platform-dependent.  This should be
+#  customized in select occassions, e.g., you want 'apache2-mpm-prefork' on
+#  Debian platforms.
+#
+class apache::install(
+  $package     = $apache::params::package,
+  $ssl_package = $apache::params::ssl_package,
+  $provider    = $apache::params::provider,
+) inherits apache::params {
 
-  package { $apache::params::package:
+  package { $package:
     ensure   => installed,
     alias    => 'apache',
-    provider => $apache::params::provider,
+    provider => $provider,
   }
 
-  if $apache::params::ssl {
+  if $ssl_package {
     # Install mod_ssl if not included with Apache package.
-    package { $apache::params::ssl_package:
+    package { $ssl_package:
       ensure   => installed,
       alias    => 'apache-ssl',
-      provider => $apache::params::provider,
+      provider => $provider,
     }
   }
 }
