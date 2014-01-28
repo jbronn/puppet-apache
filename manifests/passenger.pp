bb325f28dc777051148a80a68e367fb226e5706d
Justin Bronn <justin@counsyl.com>
2014-01-27 18:49:28 -0800

    Cleaned up `apache::passenger`, and moved everything to a single manifest.

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -24,40 +24,59 @@
 #  Passenger.  Defaults to 'apache/passenger/passenger.conf.erb'.
 #
 class apache::passenger(
-  $max_pool_size='6',
-  $max_requests=undef,
-  $pool_idle_time=undef,
-  $stat_throttle_rate=undef,
-  $template='apache/passenger/passenger.conf.erb',
-) {
-  include apache::passenger::install
+  $max_pool_size      = '6',
+  $max_requests       = undef,
+  $pool_idle_time     = undef,
+  $stat_throttle_rate = undef,
+  $template           = 'apache/passenger/passenger.conf.erb',
+) inherits apache::params {
+  include apache
+  include apache::devel
+  include ruby::passenger
+  include ruby::devel
+  include sys
+  include sys::gcc
 
-  # Create Apache Passenger module loading and configuration files.
-  $load = "${apache::params::mods_available}/passenger.load"
-  file { $load:
-    ensure  => file,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
-    content => template('apache/passenger/passenger.load.erb'),
-    require => Class['apache::passenger::install'],
-    notify  => Service['apache'],
+  case $::osfamily {
+    debian: {
+      $dev_packages = ['libcurl4-openssl-dev', 'libssl-dev', 'zlib1g-dev']
+    }
+    redhat: {
+      $dev_packages = ['libcurl-devel', 'openssl-devel', 'zlib-devel']
+    }
+    default: {
+      fail("Don't know how to install Phusion Passenger for Apache on: ${::osfamily}.\n")
+    }
   }
 
-  $conf = "${apache::params::mods_available}/passenger.conf"
-  file { $conf:
-    ensure  => file,
-    mode    => '0644',
-    owner   => 'root',
-    group   => 'root',
+  # Libraries necessary to compile Passenger.
+  package { $dev_packages:
+    ensure  => installed,
+    require => Class['sys::gcc', 'ruby::devel'],
+  }
+
+  # Compile and install passenger for Apache.
+  exec { 'install-passenger-module':
+    command     => 'passenger-install-apache2-module -a',
+    creates     => $ruby::passenger::apache_module,
+    path        => ['/usr/bin', '/bin', '/usr/local/bin'],
+    user        => 'root',
+    environment => ["HOME=${sys::root_home}"],
+    subscribe   => Class['ruby::passenger'],
+    require     => [Class['apache::config', 'apache::devel'],
+                    Package[$dev_packages]],
+  }
+
+  # Create Apache Passenger module loading and configuration files.
+  apache::mod { 'passenger':
     content => template($template),
-    require => Class['apache::passenger::install'],
-    notify  => Service['apache'],
+    path    => $ruby::passenger::apache_module,
+    require => Exec['install-passenger-module'],
   }
 
   # Enable Apache passenger module.
   apache::module { 'passenger':
     ensure  => present,
-    require => [ File[$load], File[$conf] ],
+    require => Apache::Mod['passenger'],
   }
 }
